cc40fd249d4d0d467a11e24c356461ecfd06f5f4
divya <divu333@gmail.com>
2018-12-30 15:02:24 -0700

    commit
    commit

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -9,7 +9,7 @@ import './Post.css';
 
 import Edit from './Edit/Edit';
 
-//////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *APP* COMPONENT
+//////////////// THIS COMPONENT IS BEING RENDERED IN THE *APP* COMPONENT
 
 export default class Post extends Component {
   constructor() {
@@ -47,68 +47,55 @@ export default class Post extends Component {
     if ( this.state.showMasterMenu === true ) {
       this.setState({ showMasterMenu: false });
     }
+  
   }
 
+
+
   render() {
-    // This is destructuring! You can also think of it as being written as so:
-      // const editing = this.state.editing
-      // const showMasterMenu = this.state.showMasterMenu
     const { editing, showMasterMenu } = this.state;
-
+    const { text, date } = this.props;
+    const { id ,deletePostFn} = this.props;
+  
     return (
-      // Main body of post
       <section className="Post__parent" onClick={ this.hideMasterMenu }>
-
-        {/* Three dots in top right corner */}
+  
         <div className="Post__master-controls">
           <MasterControlIcon onClick={ this.toggleMasterMenu } />
-
-          {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
+  
           <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
             <span onClick={ this.showEdit }>Edit</span>
-            <span>Delete</span>
-          </div>
+            <span onClick={ () => deletePostFn( id ) }>Delete</span> { /* Remember to destructure deletePostFn off of props or use this.props.deletePostFn */ }          </div>
         </div>
-
-        {/* This is where all the meta data of the post will go (who, when, where) */}
+  
         <div className="Post__meta-data">
           <div className="Post__profile-picture">
             <ProfileIcon />
           </div>
-
+  
           <span className="Post__name">DevMountain</span>
           <span className="Post__handle">@DevMountain</span>
-
-          <span className="Post__date">- POST DATE GOES HERE</span>
+  
+          <span className="Post__date">- { date }</span>
         </div>
-
-        {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
-            You can also think of it as being written as so:
-              if( this.state.editing === true ) {
-                <Edit ... />
-              } else {
-                <span ... ></span>
-              }
-        */}
+  
         <div className="Post__content">
           {
-            // This has been pulled off of this.state via destructuring
             editing
             ?
               <Edit text=""
                     hideEdit={ this.hideEdit } />
             :
-              <span className="Post__text">POST TEXT GOES HERE</span>
+              <span className="Post__text">{ text }</span>
           }
         </div>
-
-        {/* These are all of the cute little icons in the bottom left corner */}
+  
         <div className="Post__user-controls">
           <ReplyIcon className="Post__control-icon" />
           <FavoriteIcon className="Post__control-icon" />
           <MessageIcon className="Post__control-icon" />
         </div>
-
+  
       </section>
     )
   }
